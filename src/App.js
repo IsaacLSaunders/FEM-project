7cc3152de881195e38aed1e28088f15b31dc4326
Isaac Saunders <IsaacLSaunders@gmail.com>
2022-05-10 13:29:00 -0400

    first section completed

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Edna',
+      animal: 'Dog',
+      breed: 'Pure Bred Mutt',
+    }),
+    React.createElement(Pet, {
+      name: 'Rijken',
+      animal: 'Dog',
+      breed: 'Bouvier',
+    }),
+    React.createElement(Pet, {
+      name: 'Other Cat',
+      animal: 'Cat',
+      breed: 'Lucky Black',
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
