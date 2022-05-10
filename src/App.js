31acb3a383200a096604011b5eff217981d74012
Isaac Saunders <IsaacLSaunders@gmail.com>
2022-05-10 17:05:57 -0400

    refactored with JSX

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//REACT 18 uses createRoot instead of ReactDOM.render, version 17 will be used unless this is changed
+import { render } from 'react-dom';
+import Pet from './Pet';
 
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed),
-  ]);
-};
+//V1
+// const App = () => {
+//   return React.createElement('div', {}, [
+//     React.createElement('h1', {}, 'Adopt Me!'),
+//     React.createElement(Pet, {
+//       name: 'Edna',
+//       animal: 'Dog',
+//       breed: 'Pure Bred Mutt',
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Rijken',
+//       animal: 'Dog',
+//       breed: 'Bouvier',
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Other Cat',
+//       animal: 'Cat',
+//       breed: 'Lucky Black',
+//     }),
+//   ]);
+// };
+//ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Edna',
-      animal: 'Dog',
-      breed: 'Pure Bred Mutt',
-    }),
-    React.createElement(Pet, {
-      name: 'Rijken',
-      animal: 'Dog',
-      breed: 'Bouvier',
-    }),
-    React.createElement(Pet, {
-      name: 'Other Cat',
-      animal: 'Cat',
-      breed: 'Lucky Black',
-    }),
-  ]);
+  return (
+    <div id='My App'>
+      <h1>Adopt Me!</h1>
+      <Pet name='Edna' animal='Dog' breed='Pure Bred Mutt' />
+      <Pet name='Rijken' animal='Dog' breed='Bouvier' />
+      <Pet name='Other Cat' animal='Cat' breed='Lucky Black' />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
