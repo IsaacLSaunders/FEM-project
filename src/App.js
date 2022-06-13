1e0a7582161564aacabc8722750254f9a74369c4
Isaac Saunders <IsaacLSaunders@gmail.com>
2022-06-13 17:45:23 -0400

    restart @JSX 6/13

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+//Parcel automatically supports JSX and React, Parcel detects that you are using React and compiles automatically without imports.
+// import React from 'react';
+
 //REACT 18 uses createRoot instead of ReactDOM.render, version 17 will be used unless this is changed
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
+// import Pet from './Pet';
 
-//V1
+/*V1
 // const App = () => {
 //   return React.createElement('div', {}, [
 //     React.createElement('h1', {}, 'Adopt Me!'),
@@ -25,14 +28,13 @@ import Pet from './Pet';
 //   ]);
 // };
 //ReactDOM.render(React.createElement(App), document.getElementById('root'));
+*/
 
 const App = () => {
   return (
     <div id='My App'>
       <h1>Adopt Me!</h1>
-      <Pet name='Edna' animal='Dog' breed='Pure Bred Mutt' />
-      <Pet name='Rijken' animal='Dog' breed='Bouvier' />
-      <Pet name='Other Cat' animal='Cat' breed='Lucky Black' />
+      <SearchParams />
     </div>
   );
 };
